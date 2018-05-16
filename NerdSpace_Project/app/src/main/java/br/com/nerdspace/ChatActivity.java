6680af4db2559b58df294c90feb0d49b59c96696
Marbet Ramon <tebram.dev@gmail.com>
2018-05-16 17:36:50 -0300

    added Chat TabsBar

--- NerdSpace_Project/app/src/main/java/br/com/nerdspace/ChatActivity.java
+++ NerdSpace_Project/app/src/main/java/br/com/nerdspace/ChatActivity.java
@@ -1,13 +1,55 @@
 package br.com.nerdspace;
 
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ChatActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private ViewPager mViewPager;
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //Tabs
+        mViewPager = (ViewPager) findViewById(R.id.main_tab_ViewPager);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //verifica se o usuário esta conectado(non-null) e age de acordo.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+            Intent startIntent = new Intent(ChatActivity.this, LoginActivity.class);
+            startActivity(startIntent);
+            finish();
+        } else {
+            finish();
+            startActivity(new Intent(getApplicationContext(), ChatActivity.class));
+        }
+
     }
 }
